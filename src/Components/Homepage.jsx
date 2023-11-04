c346d31b2e8cb0e6dfa7ff8efc1bcc858168f96f
Gbenga <gbengaoluwadahunsi01@gmail.com>
2023-11-04 15:35:09 +0800

    navbar adjustment

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -14,19 +14,23 @@ import  '../index.css'
     <>
       <section  loading = "lazy" className="  h-80 md:h-screen lg:h-screen   bg-center bg-cover bg-no-repeat bk" >
         <div className="grid grid-cols-6 font-bold  p-4 lg:px-10 lg:py-3 bg-cyan-900"> 
-        <img className=" w-10 h-10 lg:w-14 rounded-full lg:h-14" src={motiva2} alt="" />
-        <div className="col-span-5 grid grid-cols-2 lg:gap-20 items-center">
-        <div className=" grid grid-cols-5 gap-3 py-3 text-cyan-50 list-none ">
-            <li className=" justify-self-start">How it works</li>
-            <li className="  col-span-2 justify-self-center">Get Motivational Quotes</li>
-            <li className="col-span-2 ">Make your design</li>
+          <img className=" w-10 h-10 lg:w-14 rounded-full lg:h-14" src={motiva2} alt="" />
+          
+          <div className="col-span-5 grid grid-cols-2 lg:gap-20 items-center">
+
+            <div className=" grid grid-cols-5  py-3 text-cyan-50 list-none ">
+                <li className=" justify-self-start">How it works</li>
+                <li className="  col-span-2 justify-self-center">Get Motivational Quotes</li>
+                <li className="col-span-2 ">Make your design</li>
+                
+            </div>          
+            <button className="items-center text-cyan-100 list-none justify-self-end bg-cyan-500 w-28 px-4  py-3 rounded-full  ">Log in</button>
             
+          </div>
+
         </div>
-        <button className="items-center text-cyan-100 list-none justify-self-end bg-cyan-500 w-28 px-4  py-3 rounded-full  ">Log in
-        </button>
-        </div>
-        </div>
-         <main className=" p-1 lg:p-10">
+
+        <main className=" p-1 lg:p-10">
             
                 <div className=" flex flex-col gap-6 text-center  mt-10 lg:mt-28">
 
