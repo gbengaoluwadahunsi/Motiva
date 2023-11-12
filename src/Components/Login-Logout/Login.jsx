6851662cdd68a50ea2f84bbf211e9b7315315df3
Gbenga <gbengaoluwadahunsi01@gmail.com>
2023-11-12 23:21:18 +0800

    responsiveness of app fixed

--- src/Components/Login-Logout/Login.jsx
+++ src/Components/Login-Logout/Login.jsx
@@ -1,24 +1,23 @@
 import { useState } from 'react';
-import { Link,useNavigate  } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { signInWithEmailAndPassword, GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from 'firebase/auth';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import image from '/public/motiva.png';
-import  google from '/src/assets/Images/Google.png'
-import { FaFacebook } from 'react-icons/fa'
-import { auth } from './Firebase'; 
+import google from '/src/assets/Images/Google.png';
+import { FaFacebook } from 'react-icons/fa';
+import { auth } from './Firebase';
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState('');
   const navigate = useNavigate();
- 
 
   const handleLogin = async (values) => {
     try {
       const userCredential = await signInWithEmailAndPassword(auth, values.email, values.password);
-      console.log('Sign up successful:', userCredential.user);
-      alert('Sign up successful');
+      console.log('Sign in successful:', userCredential.user);
+      alert('Sign in successful');
       navigate('/');
     } catch (error) {
       console.error(error);
@@ -30,9 +29,9 @@ const Login = () => {
     const provider = new GoogleAuthProvider();
     try {
       const userCredential = await signInWithPopup(auth, provider);
-       console.log('Sign up successful:', userCredential.user);
-    alert('Sign up successful');
-    navigate('/');
+      console.log('Sign in successful:', userCredential.user);
+      alert('Sign in successful');
+      navigate('/');
     } catch (error) {
       console.error(error);
       setError('Error signing in with Google');
@@ -43,9 +42,9 @@ const Login = () => {
     const provider = new FacebookAuthProvider();
     try {
       const userCredential = await signInWithPopup(auth, provider);
-       console.log('Sign up successful:', userCredential.user);
-    alert('Sign up successful');
-    navigate('/');
+      console.log('Sign in successful:', userCredential.user);
+      alert('Sign in successful');
+      navigate('/');
     } catch (error) {
       console.error(error);
       setError('Error signing in with Facebook');
@@ -53,14 +52,14 @@ const Login = () => {
   };
 
   return (
-    <div className='h-screen grid place-items-center bg-center login-background'>
-      <div className="bg-white text-center  lg:-mt-3 mx-auto grid grid-rows-5 lg:h-formHeight gap-2 p-4 rounded-lg lg:w-1/4 ">
+    <div className="h-screen grid place-items-center bg-center login-background">
+      <div className="bg-white text-center lg:-mt-3 mx-auto grid grid-rows-5 lg:h-formHeight gap-2 p-4 rounded-lg lg:w-1/4">
         <img className="w-10 h-10 lg:w-20 rounded-full border-4 border-cyan-950 mx-auto lg:h-20" src={image} alt="motiva logo" />
-        <h1 className='text-cyan-900 font-extrabold lg:text-xl'>Welcome back, Sign In</h1>
+        <h1 className="text-cyan-900 font-extrabold lg:text-xl">Welcome back, Sign In</h1>
         {error && <div className="text-red-700 font-semibold">{error}</div>}
         <Formik
           initialValues={{ email: '', password: '' }}
-          validate={values => {
+          validate={(values) => {
             const errors = {};
             if (!values.email) {
               errors.email = 'Required';
@@ -77,42 +76,56 @@ const Login = () => {
           onSubmit={handleLogin}
         >
           {({ isSubmitting }) => (
-            <Form  className='row-span-3 bg-white grid grid-row-6 gap-2 '>
-              <Field className='px-4  h-8 lg:h-14  rounded border-2 border-cyan-950 bg-gray-100 row-span-2' type="email" name="email" placeholder="Email Address" />
-              <ErrorMessage className='text-red-700 font-semibold' name="email" component="div" />
-              <div className=' w-full   bg-gray-100 rounded relative h-8 b lg:h-14'>
-                <Field className=' px-4 h-full w-full bg-gray-100 rounded border-2 border-cyan-950 row-span-2 pr-10' type={showPassword ? 'text' : 'password'} name="password" placeholder="Password" />
-                <div className='absolute top-2 lg:top-6 right-3'>
-                  {showPassword ?
-                    <FaEyeSlash className='cursor-pointer  text-cyan-950' onClick={() => setShowPassword(false)} /> :
-                    <FaEye className='cursor-pointer text-cyan-950' onClick={() => setShowPassword(true)} />
-                  }
+            <Form className="row-span-3 bg-white grid grid-row-6 gap-2">
+              <Field className="px-4 h-8 lg:h-14 rounded border-2 border-cyan-950 bg-gray-100 row-span-2" type="email" name="email" placeholder="Email Address" />
+              <ErrorMessage className="text-red-700 font-semibold" name="email" component="div" />
+              <div className="w-full bg-gray-100 rounded relative h-8 b lg:h-14">
+                <Field
+                  className="px-4 h-full w-full bg-gray-100 rounded border-2 border-cyan-950 row-span-2 pr-10"
+                  type={showPassword ? 'text' : 'password'}
+                  name="password"
+                  placeholder="Password"
+                />
+                <div className="absolute top-2 lg:top-6 right-3">
+                  {showPassword ? (
+                    <FaEyeSlash className="cursor-pointer text-cyan-950" onClick={() => setShowPassword(false)} />
+                  ) : (
+                    <FaEye className="cursor-pointer text-cyan-950" onClick={() => setShowPassword(true)} />
+                  )}
                 </div>
               </div>
-              <ErrorMessage className='text-red-700 font-semibold' name="password" component="div" />
-              <div className='grid grid-cols-2 '>
-                <label className='justify-self-start'>
-                  <Field type="checkbox" name="rememberMe" className='mr-2' />
+              <ErrorMessage className="text-red-700 font-semibold" name="password" component="div" />
+              <div className="grid grid-cols-2">
+                <label className="justify-self-start">
+                  <Field type="checkbox" name="rememberMe" className="mr-2" />
                   Remember me
                 </label>
-                <span className='justify-self-end font-semibold'>Forgot Password?</span>
+                <span className="justify-self-end font-semibold">Forgot Password?</span>
               </div>
-              <button className='bg-cyan-950 rounded  h-8 lg:h-14 text-cyan-100 lg:font-extrabold row-span-1' type="submit" disabled={isSubmitting}>
+              <button className="bg-cyan-950 rounded h-8 lg:h-14 text-cyan-100 lg:font-extrabold row-span-1" type="submit" disabled={isSubmitting}>
                 Sign In
               </button>
             </Form>
           )}
         </Formik>
-        <span className='mx-auto  lg:font-bold text-cyan-950'>OR</span>
-        <div className="flex  flex-col justify-center gap-4">
-          <button className="bg-red-500 flex flex-row items-center  justify-center lg:font-extrabold gap-4 px-4  py-2 rounded h-8 lg:h-14 text-white" onClick={handleGoogleSignIn}>
-          <img src= {google} className='-ml-4   w-5 bg-white rounded-full' sizes=''/><>Sign in with Google</>
+        <span className="mx-auto lg:font-bold text-cyan-950">OR</span>
+        <div className="flex flex-col justify-center gap-4">
+          <button
+            className="bg-red-500 flex flex-row items-center justify-center lg:font-extrabold gap-4 px-4 py-2 rounded h-8 lg:h-14 text-white"
+            onClick={handleGoogleSignIn}
+          >
+            <img src={google} className="-ml-4 w-5 bg-white rounded-full" alt="Google Logo" /><span>Sign in with Google</span>
           </button>
-          <button className="bg-blue-900 flex flex-row items-center  justify-center lg:font-extrabold gap-4 px-4  py-2 rounded h-8 lg:h-14 text-white" onClick={handleFacebookSignIn}>
-          <FaFacebook  className='lg:text-xl'/><span >Sign in with Facebook</span>
+          <button
+            className="bg-blue-900 flex flex-row items-center justify-center lg:font-extrabold gap-4 px-4 py-2 rounded h-8 lg:h-14 text-white"
+            onClick={handleFacebookSignIn}
+          >
+            <FaFacebook className="lg:text-xl" /><span>Sign in with Facebook</span>
           </button>
         </div>
-        <Link className='font-bold' to="/signUp"> No Account? <span className='font-extrabold'>Sign Up</span></Link>
+        <Link className="font-bold" to="/signUp">
+          No Account? <span className="font-extrabold">Sign Up</span>
+        </Link>
       </div>
     </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,26 +1,60 @@
-import motiva2 from   "../assets/Images/motiva2.png"
-import { Link } from 'react-router-dom'
+import motiva2 from "../assets/Images/motiva2.png";
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { FaTimesCircle } from 'react-icons/fa';
 
-const Navbar = ()  => {
+const Navbar = ({ isAuthenticated, onSignOut }) => {
+  const [navbar, setNavbar] = useState(false);
+
   return (
-    <div className="grid grid-cols-12 font-bold  p-4 lg:px-20 lg:py-3 bg-cyan-900"> 
-          <Link to="/"><img className=" w-10 h-10 lg:w-14 rounded-full lg:h-14" src={motiva2} alt="motiva-logo" /></Link>
-          
-          <div className="col-span-11 grid grid-cols-5  items-center">
+    <div className="grid grid-cols-12  lg:font-medium p-4 lg:px-20 lg:py-3 bg-cyan-900">
+      <Link to="/" className="h-14">
+        <img className="lg:w-14 lg:h-14 rounded-full" src={motiva2} alt="motiva-logo" />
+      </Link>
 
-            <div className="  col-span-4 grid grid-cols-6 gap-2 py-3 font-bold lg:text-lg text-cyan-50 ">
-                <Link to = "/" className="justify-self-center  decoration-cyan-500 decoration-2 hover:underline hover:underline-offset-8 ">Home</Link>
-                <Link to = "/howitworks" className="justify-self-center  decoration-cyan-500 decoration-2 hover:underline hover:underline-offset-8">How it works</Link>
-                <Link to = "/getquotes" className="col-span-2   justify-self-center  decoration-cyan-500 decoration-2 hover:underline hover:underline-offset-8">Get motivational quotes</Link>
-                <Link  to = "/makedesigns" className="col-span-2  decoration-cyan-500 decoration-2 hover:underline hover:underline-offset-8">Make your design</Link>
-                
-            </div>          
-            <Link to = "/login" className="justify-self-end"><button className="text-center text-cyan-100 list-none  bg-cyan-700 lg:w-30 px-6  py-3 md:py-2 lg:py-3 rounded-lg hover:scale-95 ">Sign in</button></Link>
-            
-          </div>
-
+      <section className="col-span-11 grid grid-rows-11 md:flex md:items-center relative">
+        {/* Mobile view toggle button */}
+        <div className="md:hidden absolute z-50 right-0">
+          <button
+            className="p-2 rounded-md outline-none border border-gray-400"
+            onClick={() => setNavbar(!navbar)}
+          >
+            {navbar ? (<FaTimesCircle className="text-cyan-500 text-2xl" />) : (<GiHamburgerMenu className="text-cyan-500 text-2xl" />)}
+          </button>
         </div>
-  )
-}
 
-export default Navbar
+        {/* Navigation links */}
+        <div className={` w-full top-0 md:ml-3 items-center  md:flex md:col-span-4 ${navbar ? "  space-y-2  py-20 absolute top-10 flex flex-col md:flex-row bg-cyan-600" : "hidden"}`}>
+          <Link to="/" className="md:mr-4 md:mb-0 mb-2 decoration-cyan-500 decoration-2 hover:underline hover:underline-offset-8 text-cyan-100">Home</Link>
+          <Link to="/howitworks" className="md:mr-4 md:mb-0 mb-2 decoration-cyan-500 decoration-2 hover:underline hover:underline-offset-8 text-cyan-100">How it works</Link>
+          <Link to="/getquotes" className="md:mr-4 md:mb-0 mb-2 decoration-cyan-500 decoration-2 hover:underline hover:underline-offset-8 text-cyan-100">Get motivational quotes</Link>
+          <Link to="/makedesigns" className="decoration-cyan-500 decoration-2 hover:underline hover:underline-offset-8 text-cyan-100">Make your design</Link>
+
+          {/* Sign in/out button */}
+        {isAuthenticated ? (
+          <button onClick={onSignOut} className={`md:ml-auto ${navbar ? "flex flex-col" : "hidden"} `}>
+            Sign Out
+          </button>
+        ) : (
+          <Link to="/login" className={`md:ml-auto ${navbar ? "" : ""}`}>
+            <button className="text-center text-cyan-100 bg-cyan-700 w-25  md:w-30 px-4 py-2 md:py-2 lg:py-3 rounded-lg hover:scale-95">
+              Sign In
+            </button>
+          </Link>
+        )}
+        </div>
+
+        
+      </section>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  onSignOut: PropTypes.func.isRequired,
+};
+
+export default Navbar;
