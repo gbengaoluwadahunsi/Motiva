b194915d6eb5b856709c24ffbba89a1d47d65d7c
Gbenga <gbengaoluwadahunsi01@gmail.com>
2023-11-12 23:34:44 +0800

    mobile toggle navbar fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = ({ isAuthenticated, onSignOut }) => {
 
   return (
     <div className="grid grid-cols-12  lg:font-medium p-4 lg:px-20 lg:py-3 bg-cyan-900">
-      <Link to="/" className="h-14">
-        <img className="lg:w-14 lg:h-14 rounded-full" src={motiva2} alt="motiva-logo" />
+      <Link to="/" className="lg:h-14">
+        <img className=" lg:w-14  lg:h-14 rounded-full" src={motiva2} alt="motiva-logo" />
       </Link>
 
       <section className="col-span-11 grid grid-rows-11 md:flex md:items-center relative">
@@ -21,12 +21,12 @@ const Navbar = ({ isAuthenticated, onSignOut }) => {
             className="p-2 rounded-md outline-none border border-gray-400"
             onClick={() => setNavbar(!navbar)}
           >
-            {navbar ? (<FaTimesCircle className="text-cyan-500 text-2xl" />) : (<GiHamburgerMenu className="text-cyan-500 text-2xl" />)}
+            {navbar ? (<FaTimesCircle className="text-cyan-500 text-md" />) : (<GiHamburgerMenu className="text-cyan-500 text-md" />)}
           </button>
         </div>
 
         {/* Navigation links */}
-        <div className={` w-full top-0 md:ml-3 items-center  md:flex md:col-span-4 ${navbar ? "  space-y-2  py-20 absolute top-10 flex flex-col md:flex-row bg-cyan-600" : "hidden"}`}>
+        <div className={` w-full top-0 md:ml-3 items-center  md:flex md:col-span-4 ${navbar ? "  space-y-2  py-10 absolute top-8 flex flex-col md:flex-row bg-cyan-600" : "hidden"}`}>
           <Link to="/" className="md:mr-4 md:mb-0 mb-2 decoration-cyan-500 decoration-2 hover:underline hover:underline-offset-8 text-cyan-100">Home</Link>
           <Link to="/howitworks" className="md:mr-4 md:mb-0 mb-2 decoration-cyan-500 decoration-2 hover:underline hover:underline-offset-8 text-cyan-100">How it works</Link>
           <Link to="/getquotes" className="md:mr-4 md:mb-0 mb-2 decoration-cyan-500 decoration-2 hover:underline hover:underline-offset-8 text-cyan-100">Get motivational quotes</Link>
